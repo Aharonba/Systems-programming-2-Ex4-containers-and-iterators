9fa423da65ef3f556de2189df7525d5e4996e323
Aharonba <146740959+Aharonba@users.noreply.github.com>
2024-08-13 14:49:48 +0000

    Add files via upload

---
+++ Ex4 - containers and iterators/Node.hpp
@@ -0,0 +1,23 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <vector>
+
+namespace ariel
+{
+    template <typename T>
+    class Node
+    {
+    public:
+        T value;                         // The value stored in the node
+        std::vector<Node<T> *> children; // Vector of pointers to child nodes
+
+        Node(const T &val) : value(val) {}
+
+        void add_child(Node<T> *child)
+        {
+            children.push_back(child);
+        }
+    };
+};
+#endif
